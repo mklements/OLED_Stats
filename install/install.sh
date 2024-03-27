f7d2e502240ba99a0ebc3b85a9967766f7ee4a14
cubs818 <craigmitchell18@gmail.com>
2024-03-27 06:02:30 -0500

    cleanup

--- install/install.sh
+++ install/install.sh
@@ -6,14 +6,16 @@ sudo apt-get -y install python3-pip
 sudo apt-get -y install python3.11-venv
 sudo apt-get -y install python-is-python3
 
-sudo apt-get install -y i2c-tools libgpiod-dev python3-libgpiod
+sudo apt-get -y install  i2c-tools libgpiod-dev python3-libgpiod
 
 sudo raspi-config nonint do_i2c 0
 sudo raspi-config nonint do_spi 0
 sudo raspi-config nonint do_ssh 0
 sudo raspi-config nonint disable_raspi_config_at_boot 0
 
-# cd /home/operator/oled_stats
+mkdir /home/controlpanel/control-panel
+cd /home/controlpanel/control-panel
+git clone https://github.com/ctus-dev/control-panel.git
 python -m venv .venv --system-site-packages
 source .venv/bin/activate
 pip install --upgrade -r requirements.txt
