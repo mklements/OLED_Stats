f5cf2eefa9047fdb6d240d9ed07cd6457e94034c
cubs818 <craigmitchell18@gmail.com>
2024-04-09 07:33:14 -0500

    add update

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # CTUS Smartrack PI
 
+## Install
+
 ```
 cd ~
 git clone https://github.com/ctus-dev/smartrack-pi.git
 cd smartrack-pi
 chmod +x install/install.sh
 install/install.sh
+```
+
+## Update
+
+```
+cd ~
+sudo rm -d -r smartrack-pi
+git clone https://github.com/ctus-dev/smartrack-pi.git
+cd smartrack-pi
+chmod +x install/update.sh
+update/install.sh
 ```
--- install/update.sh
+++ install/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 function set_alias() {
   local alias=$1
   local alias_target=$2
@@ -11,27 +12,6 @@ function set_alias() {
   fi
 }
 
-set_config "usb_max_current_enable=1"
-
-sudo apt-get update -y
-sudp apt-get full-upgrade -y
-
-sudo apt-get -y install python3.11
-sudo apt-get -y install python3-pip
-sudo apt-get -y install python3.11-venv
-sudo apt-get -y install python-is-python3
-
-sudo apt-get -y install  i2c-tools libgpiod-dev python3-libgpiod
-
-sudo raspi-config nonint do_i2c 0
-sudo raspi-config nonint do_spi 0
-sudo raspi-config nonint do_ssh 0
-sudo raspi-config nonint disable_raspi_config_at_boot 0
-
-sudo nmcli c add ifname eth0 type ethernet con-name ipstatic
-sudo nmcli c add ifname eth0 type ethernet con-name dhcp
-sudo nmcli con mod ipstatic ipv4.method manual ipv4.addresses 192.168.1.241/24 ipv4.gateway 192.168.1.1 ipv4.may-fail no ipv6.method disabled connection.autoconnect no connection.autoconnect-priority -1
-sudo nmcli con mod dhcp ipv4.method auto ipv4.addresses '' ipv4.gateway '' ipv4.may-fail no ipv4.dhcp-timeout 20 ipv6.method disabled connection.autoconnect no connection.autoconnect-priority -1 connection.autoconnect-retries 3
 
 python -m venv .venv --system-site-packages
 source .venv/bin/activate
@@ -48,8 +28,6 @@ sudo systemctl start stats.service
 sudo systemctl start button.service
 
 # webserver
-sudo apt-get -y install apache2
-sudo apt-get -y install php
 
 sudo cp install/apache/smartrack-config.conf /etc/apache2/sites-available/smartrack-config.conf
 sudo cp install/apache/envvars /etc/apache2/envvars
