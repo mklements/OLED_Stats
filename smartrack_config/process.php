fb05d6548879c4238aeaf2603a6d07f9a3cd6aaa
cubs818 <craigmitchell18@gmail.com>
2024-04-10 11:36:50 -0500

    add net to command

--- smartrack_config/process.php
+++ smartrack_config/process.php
@@ -3,7 +3,7 @@
 $ip_address = $_POST['ip_address'];
 $gateway =  $_POST['gateway'];
 
-$output = shell_exec("sudo /home/smartrack/smartrack-pi/.venv/bin/python /home/smartrack/smartrack-pi/smartrack_pi/cli.py static $ip_address $gateway") or die("Failed Running static change");
+$output = shell_exec("sudo /home/smartrack/smartrack-pi/.venv/bin/python /home/smartrack/smartrack-pi/smartrack_pi/cli.py net static $ip_address $gateway") or die("Failed Running static change");
 
 exit;
 ?>
