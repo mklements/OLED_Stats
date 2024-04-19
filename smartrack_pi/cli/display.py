6044934f721fbe1a8121cd2900357489b013f040
Smartrack <Smartrack>
2024-04-19 10:42:47 -0500

    logic for long text

--- smartrack_pi/cli/display.py
+++ smartrack_pi/cli/display.py
@@ -1,21 +1,25 @@
-import typer
 from typing import Annotated
 
-from smartrack_pi.display import stats_status, display_text
+import typer
+
+from smartrack_pi.display import display_text, stats_status
+
 app = typer.Typer()
 
 
 @app.command()
-def stats(enable: Annotated[bool, typer.Option(help="Turns Stats Loop on or off")] = True):
+def stats(
+    enable: Annotated[bool, typer.Option(help="Turns Stats Loop on or off")] = True
+):
     print(f"Setting stats display to {enable}")
     stats_status(enable)
 
 
 @app.command()
-def message(message: Annotated[str, typer.Argument(help="Message to display")]):    
-    print(f"Displaying Message: {message}")
+def message(text: Annotated[str, typer.Argument(help="Message to display")]):
+    print(f"Displaying Message: {text}")
     print("Warning:  Stats will be disabled, to re-enable 'smartrack display stats'")
-    display_text(message)
+    display_text(text)
 
 
 if __name__ == "__main__":
