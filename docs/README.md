ea7b1cbb3e212c4d0c6d36bf18b1ea870720ca06
Smartrack <Smartrack>
2024-04-21 07:09:22 -0500

    readme

--- docs/README.md
+++ docs/README.md
@@ -13,7 +13,64 @@ install/install.sh
 
 ## Operation
 
+### Device Button
+
+-   Push once to toggle network modes between DHCP and Static
+-   Hold for 5 seconds for factory reset. Deletes all Companion user configs, resets default static IP, sets network mode to DHCP, reboots unit
+
+---
+
+### Web GUI
+
+_Web GUI address x.x.x.x/settings_
+
+-   Companion - Redirects to ip:8000 for Companion control
+
+-   Files
+
+    -   Backup - prefixes 'user-' and stores file locally
+
+        > <img src="assets/backup.png" alt="backup" width="500"/>
+
+    -   Restore - select file to restore to, includes system and user created files
+
+        > <img src="assets/restore.png" alt="restore" width="500"/>
+
+    -   Delete - delete user created configs
+
+        > <img src="assets/delete.png" alt="delete" width="500"/>
+
+-   Network
+
+    -   Set Static IP - Changes mode to static and sets address
+
+        > <img src="assets/network.png" alt="network" width="500"/>
+
+    -   Bitmask Calculator - computes subnet mask address from bit mask
+
+        > <img src="assets/bitmask.png" alt="bitmask" width="500"/>
+
+-   System
+
+    -   Update (Requires Internet) - Gets latest web gui, button, and display software. No changes to companion or stored user files
+
+        > <img src="assets/update.png" alt="update" width="500"/>
+
+    -   Reset - Same function as holding button for 5 seconds
+
+        > <img src="assets/reset.png" alt="reset" width="500"/>
+
+---
+
 ### CLI
+
+| Title             | Command                                        | Description                                         |
+| ----------------- | ---------------------------------------------- | --------------------------------------------------- |
+| Version           | `smartrack --version`                          | Returns current cli version                         |
+| Update            | `smartrack update`                             | Software Update                                     |
+| Factory Reset     | `smartrack factory`                            | Factory Reset                                       |
+| Display - Stats   | `smartrack display stats --enable/--no-enable` | Runs system stats on device screen                  |
+| Display - Message | `smartrack display message *text*`             | Stops Stats and displays a message on device screen |
 
 -   Network Mode
 
@@ -43,20 +100,22 @@ smartrack display message "A Test Message"
 smartrack display message "A Test Message+Line 2"
 ```
 
-- Software Update
+-   Software Update
+
 ```
 smartrack update
 ```
 
-- Factory Reset
+-   Factory Reset
+
 ```
 smartrack factory
 ```
 
 ### Companion Config
 
-Database is contained at /home/companion/.config/companion-nodejs/v3.2/db (note the version number if image changes).  This command will copy the current config 
+Database is contained at /home/companion/.config/companion-nodejs/v3.2/db (note the version number if image changes). This command will copy the current config
 
 ## Notes
 
-- If companion image changes database folder needs to be changed in install.sh
+-   If companion image changes database folder needs to be changed in install.sh
--- smartrack_pi/settings/software.py
+++ smartrack_pi/settings/software.py
@@ -1,30 +1,31 @@
-import subprocess
-from smartrack_pi.display import show
-import shutil
-import pathlib
-from datetime import datetime
 import os
+import pathlib
+import shutil
+import subprocess
+from datetime import datetime
 
+from smartrack_pi.display import show
+from smartrack_pi.net import adaptor
 
 COMPANION_DB = "/home/companion/.config/companion-nodejs/v3.2/db"
 SYSTEM_CONFIGS = ["default"]
+
 
 def get_companion_configs():
     folder = "/home/smartrack/smartrack-pi/companion"
     return [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
 
+
 def _run_process(commands):
-    return subprocess.run(
-        commands,
-        check=False,
-        cwd="/home/smartrack/smartrack-pi"
-    )
+    return subprocess.run(commands, check=False, cwd="/home/smartrack/smartrack-pi")
+
 
 def check_file(file_name):
-    file_path =f"/home/smartrack/smartrack-pi/companion/{file_name}"
+    file_path = f"/home/smartrack/smartrack-pi/companion/{file_name}"
     if os.path.isfile(file_path):
         return True
     return False
+
 
 def update():
     print("Updating...")
@@ -37,48 +38,64 @@ def update():
     print("Update complete.")
     return
 
-def factory_reset():    
+
+def factory_reset():
     print("Resetting...")
     show.text("Factory Resetting Companion...")
     for config in get_companion_configs():
         if not config.startswith("system-"):
-            _run_process(["sudo", "rm", f"/home/smartrack/smartrack-pi/companion/{config}"])
-    _run_process(["sudo", "cp", "/home/smartrack/smartrack-pi/companion/system-default", "/home/companion/.config/companion-nodejs/v3.2/db"])
+            _run_process(
+                ["sudo", "rm", f"/home/smartrack/smartrack-pi/companion/{config}"]
+            )
+    _run_process(
+        [
+            "sudo",
+            "cp",
+            "/home/smartrack/smartrack-pi/companion/system-default",
+            "/home/companion/.config/companion-nodejs/v3.2/db",
+        ]
+    )
+    address = adaptor.Address()
+    address.factory_reset()
     _run_process(["sudo", "reboot"])
 
     print("Update complete.")
     return
 
+
 def backup_companion_file(file_name):
-    file_name= file_name.replace(" ", "").lower()
+    file_name = file_name.replace(" ", "").lower()
     print("Backing up Companion...")
     if file_name.find("system-") != -1:
         return f"File Name not allowed to start with 'system-'"
     elif not file_name.startswith("user-"):
-        file_name = f"user-{file_name}"    
-    repo_db =f"/home/smartrack/smartrack-pi/companion/{file_name}"
+        file_name = f"user-{file_name}"
+    repo_db = f"/home/smartrack/smartrack-pi/companion/{file_name}"
     _run_process(["sudo", "cp", COMPANION_DB, repo_db])
-    return("Back up Complete")
+    return "Back up Complete"
+
 
 def restore_companion_file(file_name):
     print("Updating Companion...")
-    file_path =f"/home/smartrack/smartrack-pi/companion/{file_name}"
+    file_path = f"/home/smartrack/smartrack-pi/companion/{file_name}"
     show.text(f"Restoring Companion File: {file_name} & Rebooting")
     _run_process(["sudo", "cp", file_path, COMPANION_DB])
     _run_process(["sudo", "reboot"])
-    return(f"Restoring {file_name}")
+    return f"Restoring {file_name}"
+
 
 def delete_companion_file(file_name):
     print("Deleting Companion File...")
-    file_path =f"/home/smartrack/smartrack-pi/companion/{file_name}"
+    file_path = f"/home/smartrack/smartrack-pi/companion/{file_name}"
     _run_process(["sudo", "rm", file_path])
-    return(f"Deleted {file_name}")
+    return f"Deleted {file_name}"
+
 
 def push_companion_config(file_name):
     print("Saving System File...")
     if not file_name.startswith("user-"):
-        file_name = f"user-{file_name}" 
-    user_db =f"/home/smartrack/smartrack-pi/companion/{file_name}"
+        file_name = f"user-{file_name}"
+    user_db = f"/home/smartrack/smartrack-pi/companion/{file_name}"
     file_name = file_name.lower().replace("user-", "system-")
     system_db = f"/home/smartrack/smartrack-pi/companion/{file_name}"
 
@@ -86,5 +103,4 @@ def push_companion_config(file_name):
     _run_process(["git", "add", system_db])
     _run_process(["git", "commit", "-m", "companion update", system_db])
     _run_process(["git", "push", "origin", "master"])
-    return("Update complete.")
-
+    return "Update complete."
